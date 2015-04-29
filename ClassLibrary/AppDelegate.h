41d1b50e832f902e53830e2197e545f5374dfee6
waikeungshen <waikeungshen@gmail.com>
2015-04-29 16:12:12 +0800

    Initial Commit

---
+++ ClassLibrary/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ClassLibrary
+//
+//  Created by waikeungshen on 15/4/29.
+//  Copyright (c) 2015年 waikeungshen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
